6b69c69ffb624a29c3bce12cff5fc33fa1e607c7
ice <haozhiyx@outlook.com>
2019-09-04 09:34:28 +0800

    x264

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
 
+#include "rtmp.h"
+#include "x264.h"
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_ice_rtmpdemo_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char version[100];
+    sprintf(version,"librtmp version:%d",RTMP_LibVersion());
+    x264_picture_t *picture = new x264_picture_t;
+    return env->NewStringUTF(version);
 }
